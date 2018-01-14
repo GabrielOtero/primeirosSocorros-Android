ed53ef1535c96a428d0fe0e7937cd1e977c3b753
Gabriel Otero <otr.gabriel@gmail.com>
2018-01-13 23:52:54 -0200

    seizure

--- app/src/main/java/br/unicamp/primeirossocorros/util/Constants.java
+++ app/src/main/java/br/unicamp/primeirossocorros/util/Constants.java
@@ -9,4 +9,12 @@ public class Constants {
     public static int Q2_TRAUMA_CORRECT_ANSWER = R.id.opt2_check;
     public static int Q3_TRAUMA_CORRECT_ANSWER = R.id.opt4_check;
 
+    public static int Q1_CHOKING_CORRECT_ANSWER = R.id.opt1_check;
+    public static int Q2_CHOKING_CORRECT_ANSWER = R.id.opt3_check;
+    public static int Q3_CHOKING_CORRECT_ANSWER = R.id.opt2_check;
+
+    public static int Q1_SEIZURE_CORRECT_ANSWER = R.id.opt1_check;
+    public static int Q2_SEIZURE_CORRECT_ANSWER = R.id.opt3_check;
+    public static int Q3_SEIZURE_CORRECT_ANSWER = R.id.opt2_check;
+
 }
